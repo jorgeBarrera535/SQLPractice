ebcf620ca70f3c0f061f1a3c1b71d44e41c62383
jorgeBarrera535 <32499603+jorgeBarrera535@users.noreply.github.com>
2018-04-10 17:33:21 -0500

    Update TVShows.py

--- TVShows.py
+++ TVShows.py
@@ -9,3 +9,24 @@ VALUES (1, "Naruto", 3),
 (3, "One Piece", 2);
 
 SELECT * FROM tv_shows;
+
+CREATE TABLE dogs (
+  name TEXT,
+  breed TEXT,
+  contact_number INT,
+  date_lost TEXT,
+  last_location TEXT,
+  picture_URL TEXT,
+  Reward INT,
+  Owners_name TEXT,
+  Address TEXT,
+  dog_description TEXT,
+  Found Bool
+);
+
+INSERT INTO dogs (name, breed, contact_number, date_lost, last_location, picture_URL, Reward, Owners_name, Address, dog_description, Found)
+VALUES ("Sr.Fluffy Boots", "Blood Hound", "(555)555-5555", "3/10/2018", "1234 Hackberry Drv.", "
+("cujo", "Pitbull", 25),
+("Bubbles", "Yorkie", 5);
+
+
